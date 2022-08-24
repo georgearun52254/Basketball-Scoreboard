09896c987b735b9d5f56815649d7d27b24aaf63f
georgearun52254 <111888742+georgearun52254@users.noreply.github.com>
2022-08-24 15:37:58 +0530

    Add Basketball Scoreboard files via upload

---
+++ index.js
@@ -0,0 +1,60 @@
+let homeEl=document.getElementById("home-el")
+let guestEl=document.getElementById("guest-el")
+let resultEl=document.getElementById("result-el")
+let winnerEl=document.getElementById("winner-el")
+let homescore=0
+let guestscore=0
+function incrementH1(){
+  homescore+=1
+  homeEl.textContent=homescore
+}
+function incrementH2(){
+  homescore+=2
+  homeEl.textContent=homescore
+}
+function incrementH3(){
+  homescore+=3
+  homeEl.textContent=homescore
+}
+function incrementG1(){
+  guestscore+=1
+  guestEl.textContent=guestscore
+}
+function incrementG2(){
+  guestscore+=2
+  guestEl.textContent=guestscore
+}
+function incrementG3(){
+  guestscore+=3
+  guestEl.textContent=guestscore
+}
+function resetH() {
+  homescore=0
+  homeEl.textContent=homescore
+
+}
+function resetG() {
+  guestscore=0
+  guestEl.textContent=guestscore
+}
+function newgame() {
+   homescore=0
+  homeEl.textContent=homescore
+  guestscore=0
+  guestEl.textContent=guestscore
+}
+let scores="0"+" - "+"0"
+function saveScores(){
+  scores= homescore+" - "+guestscore
+  resultEl.textContent=scores
+ if (homescore<guestscore) {
+  winner="GUEST"
+ }
+ else{
+  winner="HOME"
+ }
+ if (homescore=guestscore) {
+  winner="DRAW"
+ }
+  winnerEl.textContent=winner
+}
